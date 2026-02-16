0c88c6ecdde05d23799ef2358e61afec2a79e79d
AnkurChouhan123 <ankurchouhan265@gmail.com>
2026-02-16 10:58:03 +0530

    Time: 0 ms (100%), Space: 42.4 MB (71.14%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int res = 0;
+
+        for(int i = 0;i<31;i++){
+            res=res|(n&1);
+            res = res<<1;
+            n=n>>1;
+        }
+
+        return res;
+    }
+}
