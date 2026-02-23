07ec06008ccb64661f3df2db3e94b1d34f46216b
AnkurChouhan123 <ankurchouhan265@gmail.com>
2026-02-23 21:55:00 +0530

    Time: 14 ms (36.17%), Space: 112.4 MB (5.17%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,15 +1,21 @@
 class Solution {
     public int minSteps(int n) {
+        dp = new Integer[n+1][n+1];
+        // for(int[] a : dp) Arrays.fill(a,-1);
         if (n == 1)
             return 0;
         return solve(n, 0, 1);
     }
+
+    Integer[][] dp;
 
     int solve(int n, int available, int onScreen) {
 
         if (onScreen == n)
             return 0;
             if(onScreen > n) return (int)1e7;
+
+            if(dp[available][onScreen] != null) return dp[available][onScreen];
 
         int paste = (int) 1e7;
         if (available > 0) {
@@ -20,6 +26,7 @@ class Solution {
              copy = 1 + solve(n, onScreen, onScreen);
         
 
-        return Math.min(copy, paste);
+        dp[available][onScreen] = Math.min(copy, paste);
+        return dp[available][onScreen];
     }
 }
