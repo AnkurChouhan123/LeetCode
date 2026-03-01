7256ccca2cdde6ba493daedddc1395ec3e6ff4ba
AnkurChouhan123 <ankurchouhan265@gmail.com>
2026-03-01 22:54:52 +0530

    Time: 0 ms (100%), Space: 49.4 MB (68.03%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -15,20 +15,17 @@
  */
 class Solution {
     public int countNodes(TreeNode root) {
+        return solve(root)/2;
+    }
 
+    int solve(TreeNode root){
         if(root == null) return 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        int count = 0;
-        while(!q.isEmpty()){
-            TreeNode node = q.poll();
-            count++;
 
-            if(node.left!=null) q.add(node.left);
-            if(node.right!=null) q.add(node.right);
-        }
+        int left = 1 + solve(root.left);
+        int right = 1+  solve(root.right);
+          
+          return left + right;
 
-        return count;
     }
 
 
