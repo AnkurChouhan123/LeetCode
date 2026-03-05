6d150fe51f3f58b2dfd11f3e3a7b1ae2c9c8ccfd
AnkurChouhan123 <ankurchouhan265@gmail.com>
2026-03-05 22:20:21 +0530

    Time: 5 ms (53.53%), Space: 43.6 MB (91.05%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.java
@@ -1,89 +1,30 @@
 class Solution {
     public int minOperations(String s) {
-      
-        int last1 = 0;
-        int count1 = 0; // 1 group will track first char as 0
 
-        int last2 = 1;
-        int count2 = 0; // 2 group will track first char as 1
+        int last1 = 0, count1 = 0;
+        int last2 = 1, count2 = 0;
 
-        if (s.charAt(0) == '0')
-            count2++;
-        else {
-            count1++;
-        }
+        if (s.charAt(0) == '0') count2++;
+        else count1++;
 
         for (int i = 1; i < s.length(); i++) {
             int num = s.charAt(i) - '0';
-            if (num == 1) {
-                if (last1 == 0) {
-                    last1 = 1;
-                }else {
-                    count1++;
-                    last1 = 0;
-                }
 
-                if(last2 == 0){
-                    last2 = 1;
-                }
-                else{
-                  count2++;
-                  last2 = 0;
-                }
+            if (num == last1) {
+                count1++;
+                last1 = 1 - last1;
+            } else {
+                last1 = num;
             }
 
-            else {
-                if (last1 == 0) {
-                    count1++;
-                    last1 = 1;
-                } else {
-                    last1 = 0;
-                }
-
-                if (last2 == 0) {
-                    count2++;
-                    last2 = 1;
-                } else {
-                    last2 = 0;
-                }
+            if (num == last2) {
+                count2++;
+                last2 = 1 - last2;
+            } else {
+                last2 = num;
             }
         }
 
-        return Math.min(count1,count2);
+        return Math.min(count1, count2);
     }
-
-    // firstCharacter will 1
-    // int secondWay(String s) {
-    //     int last;
-    //     int count = 0;
-    //     if (s.charAt(0) == '1')
-    //         last = 1;
-    //     else {
-    //         last = 1;
-    //         count++;
-    //     }
-
-    //     for (int i = 1; i < s.length(); i++) {
-    //         int num = s.charAt(i) - '0';
-    //         if (num == 1) {
-    //             if (last == 0) {
-    //                 last = 1;
-    //             } else {
-    //                 count++;
-    //                 last = 0;
-    //             }
-    //         }
-
-    //         else {
-    //             if (last == 0) {
-    //                 count++;
-    //                 last = 1;
-    //             } else {
-    //                 last = 0;
-    //             }
-    //         }
-    //     }
-
-    //     return count;
-    // }
 }
